c08a5328e188d7a4683ff5e90d95efecad903cce
Srihariaditha <adithasrihari@gmail.com>
2022-01-11 14:49:21 +0530

    Added .env file for settings

--- sw-cms/config/database.js
+++ sw-cms/config/database.js
@@ -4,12 +4,12 @@ module.exports = ({ env }) => ({
    default: {
     connector: 'mongoose',
     settings: {
-      host: env('DB_MONGO_URL', '127.0.0.1'),
+      host: env('DATABASE_HOST', '127.0.0.1'),
       srv: env.bool('DATABASE_SRV', false),
-      port: env.int('DB_PORT', 27017),
+      port: env.int('DATABASE_PORT', 27017),
       database: env('DATABASE_NAME', 'sw-strapi'),
-      username: env('DB_USER_NAME', null),
-      password: env('DB_PASSWORD', null)
+      username: env('DATABASE_USERNAME', null),
+      password: env('DATABASE_PASSWORD', null)
     },
     options: {
       authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
