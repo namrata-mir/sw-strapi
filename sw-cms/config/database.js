2594ede49c68b5f5d7154b2206bfb137068e9b94
Srihariaditha <adithasrihari@gmail.com>
2022-01-11 14:34:58 +0530

    Adding database configurations to set it to  mongo

--- sw-cms/config/database.js
+++ sw-cms/config/database.js
@@ -1,19 +1,20 @@
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'postgres',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'strapi-database'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
-      },
-      options: {
-        ssl: false,
-      },
+   default: {
+    connector: 'mongoose',
+    settings: {
+      host: env('DB_MONGO_URL', '127.0.0.1'),
+      srv: env.bool('DATABASE_SRV', false),
+      port: env.int('DB_PORT', 27017),
+      database: env('DATABASE_NAME', 'sw-strapi'),
+      username: env('DB_USER_NAME', null),
+      password: env('DB_PASSWORD', null)
     },
-  },
+    options: {
+      authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
+      ssl: env.bool('DB_SSL_ENABLE', false),
+    },
+   },
+ },
 });
